295d6a3f03809bb27e297cfde13d52a8117a5522
tillybeamen <till.pugh@gmail.com>
2023-03-12 12:40:19 -0700

    get on that ride

---
+++ getOnThatRide.js
@@ -0,0 +1,26 @@
+function getOnThatRide(height, age){
+    var min_height = 42;
+    var min_age = 10;
+    if ( height >= min_height &&  age >= min_age) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.")
+    }
+    
+}
+getOnThatRide(42, 31)
+
+// Stretch feature 2
+function getOnThatRide2(height, age){
+    var min_height = 42;
+    var min_age = 10;
+    if ( height >= min_height ||  age >= min_age) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.")
+    }
+    
+}
+getOnThatRide2(42, 3)
